d12a0fefcbd9bf7f8947c701f3d6c07b0281bf1d
Jonathan Schnabel <jonathan.schnabel31@gmail.com>
2018-10-25 19:51:22 +0100

    add test file in input folder

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #
 #python ./src/h1b_counting.py ./input/h1b_input.csv ./output/top_10_occupations.txt ./output/top_10_states.txt
 
-python3 ./src/h1b_stats.py H-1B_Disclosure_Data_FY15_Q4 top_10_occupations.txt top_10_states.txt
+python3 ./src/h1b_stats.py h1b_input.csv top_10_occupations.txt top_10_states.txt
